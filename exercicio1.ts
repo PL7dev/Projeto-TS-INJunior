076be031bd74d769fc2603c1881ec55d5fa5105b
PedroLucas <pedrolucasamotta0@gmail.com>
2026-02-10 10:33:57 -0300

    Resolução do exercício 1

---
+++ exercicio1.ts
@@ -0,0 +1,9 @@
+let total = 100;
+
+const adicionar = (a = 0, b = 0) => {
+  return a + b;
+};
+
+// Exemplo
+const resultado = adicionar(20, 30); // Resultado 50
+console.log('exercicio 1 -> resultado =', resultado);
